db727b259c20e881f54ee1910e970f5dcdbe4f7a
yongjun21 <yjthong.2009@smu.edu.sg>
2017-11-30 00:35:01 +0800

    Improve export function

--- src/export.js
+++ src/export.js
@@ -8,7 +8,7 @@ const test = [{
   label: 'age-over-65',
   theme: 'Resident Population By Age & Planning Area',
   year: '2016',
-  filters: ['25', 'Over 85']
+  filters: ['26', 'Over 85']
 }]
 
 const cache = {
@@ -29,20 +29,23 @@ export default function exportFeatures (features) {
 
   features.forEach(feature => {
     const theme = themes.filter(theme => theme.label === feature.theme)[0]
-    if (!theme) return
+    if (!theme) throw new Error('Theme not found')
     const year = feature.year
-    if (!theme.mapping[year]) return
+    if (!theme.mapping[year]) throw new Error('Year not found')
     const source = theme.sources[theme.mapping[year].source]
     const control = theme.controls[theme.mapping[year].control]
-    const filters = []
 
-    control.items.forEach((item, i) => {
-      if (feature.filters instanceof Array) {
+    let filters
+    if (control.type === 'radio') {
+      filters = control.items.filter(item => item.label === feature.filters)[0]
+      if (!filters) throw new Error('Filters not found')
+    } else {
+      filters = []
+      control.items.forEach((item, i) => {
         if (feature.filters.indexOf(item.label) > -1) filters.push(i)
-      } else {
-        if (feature.filters === item.label) filters.push(i)
-      }
-    })
+      })
+      if (control.type === 'range' && filters.length !== 2) throw new Error('Filters not found')
+    }
 
     const heatmap = cache.load(source)
 
